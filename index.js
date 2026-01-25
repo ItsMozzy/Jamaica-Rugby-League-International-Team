4e035ef0b7e583c4d73d882cbd6d9daf55a1bb8f
ItsMozzy <masonhylton@outlook.com>
2026-01-25 22:30:30 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+function home(){
+    document.getElementById("home").scrollIntoView({behavior: "smooth" });
+}
+
+function about(){
+    document.getElementById("about").scrollIntoView({behavior: "smooth" });
+
+    
+}
+
+function team(){
+    document.getElementById("team").scrollIntoView({behavior: "smooth" });
+}
+
+function sponsors(){
+    document.getElementById("sponsor").scrollIntoView({behavior: "smooth" });
+}
